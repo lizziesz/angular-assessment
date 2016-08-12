835dcfe704b551e122d144ac2c38406071c336b9
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-08-12 11:11:21 -0600

    Checking in to commit

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,29 +1,55 @@
 var app = angular.module('capstoneBrochure', ['ngRoute']);
 
-// app.config(function($routeProvider){
-//   $routeProvider
-//     .when('/', {
-//       templateUrl: 'routes/home.html'
-//       // controller: 'PiratesController'
-//     })
-//     // .when('/add', {
-//     //   templateUrl: 'views/add.html',
-//     //   controller: 'PiratesController'
-//     // })
-// })
-
 app.config(function($routeProvider){
   $routeProvider
     .when('/', {
-      templateUrl: 'index.html'
+      templateUrl: 'routes/home.html'
       // controller: 'RedditController'
+    })
+    .when('/sample', {
+      templateUrl: 'routes/sample.html'
     })
     .otherwise( { redirectTo: '/' } );
 });
 
-app.directive('gsHome', function(){
+app.directive('gsSampleSearch', function(){
   return {
     restrict: 'E',
-    templateUrl: 'routes/home.html'
+    templateUrl: 'routes/search.html'
   }
-})
+});
+
+app.controller('capstoneController', ['$scope', '$http', function($scope, $http) {
+  $scope.view = {};
+  $scope.view.showSampleSearch = false;
+
+  $scope.view.searchBooks = function(searchParam) {
+    // $http.get('https://www.googleapis.com/books/v1/volumes?q=' + searchParam).then(function(data) {
+    //   console.log(data.data.items);
+    //   $scope.view.data = data.data.items;
+    // })
+    // var url = "https://www.goodreads.com/search/index.json?key=KRvoOkz3phhD77S3dn5Ug&q=" + searchParam;
+    //
+    // $http.get("https://query.yahooapis.com/v1/public/yql?q=" + "select Status.presence from xml where url = " + url + "&format=json")
+    //     // {
+    //     //     q: "select Status.presence from xml where url = " + url + "and Status.presence.lang = 'en'",
+    //     //     format: "xml"
+    //     // })
+    //     .then(function(data) {
+    //       console.log(data);
+    //     })
+        // function(xml){
+        //     // contains XML with the following structure:
+        //     // <query>
+        //     //   <results>
+        //     //     <GoodreadsResponse>
+        //     //        ...
+        //     console.log(xml);
+        // }
+    // ); request(url).pipe(res)
+    $http.get('https://www.goodreads.com/search/index.json?key=KRvoOkz3phhD77S3dn5Ug&q=' + searchParam).then(function(data) {
+      console.log(data);
+    })
+
+  }
+}])
